87ab8480bcec04abc460ee2598f903e7f678b3d2
wero1414 <werowero.edy@gmail.com>
2017-06-09 19:38:18 -0500

    inital commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,65 @@
+function onScript(){
+
+	var socket = io.connect(window.location.href);
+
+
+	var mymap = L.map('mimapa',{
+		 center: [19.210216666666668, -96.17367333333334]
+		,zoom:16
+	});
+
+	tiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; 2016 OpenStreetMap contributors, Sacitec &copy; 2016 Electronic Cats',
+		key: 'BC9A493B41014CAABB98F0471D759707'
+
+	});
+
+	mymap.addLayer(tiles);
+
+	mymap.locate({
+		enableHighAccuracy: true
+	});
+
+	mymap.on('locationfound', onlocation);
+
+	socket.on('coords:user', onRecive);
+
+	function onRecive(data){
+		console.log(data);
+		var pos = data.latlng;
+		var computer = L.marker([pos.lat,pos.lng]);
+		mymap.addLayer(computer);
+		computer.bindPopup("Usuarios aqui");
+	}
+
+
+	function onlocation(position){
+		//console.log(position);
+		var pos = position.latlng;
+		var computer = L.marker([pos.lat,pos.lng]);
+		mymap.addLayer(computer)
+		computer.bindPopup("Computadora").openPopup();
+		socket.emit('coords:me', {latlng: pos});
+
+	}
+
+	var gps = L.marker([19.210216666666668, -96.17367333333334]);
+	mymap.addLayer(gps);
+	var posizioni = [];
+
+	socket.on('coords:gps', function (data) {
+		console.log(data);
+		var pos = data.latlng;
+		gps.setLatLng([pos.lat,pos.lng]).update();
+		posizioni.push(data.latlng);
+		gps.bindPopup( "RFM95").openPopup();
+		var polyline = L.polyline(posizioni, {color: 'red'}).addTo(mymap);
+		mymap.fitBounds(polyline.getBounds());
+
+	});
+
+}
+
+$(document).on('ready',onScript);
+		//Lat: 19.210216666666668 Lon: -96.17367333333334
